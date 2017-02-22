41b9c9acdddb53ceaa61604d39f21b6b925afcc0
Jitae Kim <originerd@gmail.com>
2017-02-22 22:39:07 +0900

    Add navigationResetToFirstScreen thunk

--- src/actions/navigation.js
+++ src/actions/navigation.js
@@ -1,3 +1,4 @@
+import firebase from '../lib/firebase';
 import {
   NAVIGATION_GOBACK,
   NAVIGATION_NAVIGATE,
@@ -19,3 +20,14 @@ export const navigationReset = (index, routes) => ({
   routes,
   type: NAVIGATION_RESET,
 });
+
+export const navigationResetToFirstScreen = () =>
+  (dispatch) => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(navigationReset(0, [{ key: 'Home_0', routeName: 'Home' }]));
+      } else {
+        dispatch(navigationReset(0, [{ key: 'Sessions_0', routeName: 'Sessions' }]));
+      }
+    });
+  };
