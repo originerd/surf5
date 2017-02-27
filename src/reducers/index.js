9fca9dc0cc984201f9af9e52a503ebb492ec682c
Jitae Kim <originerd@gmail.com>
2017-02-27 23:54:08 +0900

    Add users reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import navigation from './navigation';
 import tab from './tab';
 import timeline from './timeline';
+import users from './users';
 
 const reducers = combineReducers({
   navigation,
   tab,
   timeline,
+  users,
 });
 
 export default reducers;
