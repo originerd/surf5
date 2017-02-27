f7e43aaad39886a92438f950f3195e74cdd65b35
Jitae Kim <originerd@gmail.com>
2017-02-27 22:41:07 +0900

    Add timeline reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import navigation from './navigation';
 import tab from './tab';
+import timeline from './timeline';
 
 const reducers = combineReducers({
   navigation,
   tab,
+  timeline,
 });
 
 export default reducers;
