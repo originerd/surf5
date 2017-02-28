165a5037871ad7602d36a134a8123d2d571fc524
Jitae Kim <originerd@gmail.com>
2017-03-01 01:22:55 +0900

    Add usersLoad to users action

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,23 @@
-/* eslint import/prefer-default-export: "off" */
-
+import firebase from '../lib/firebase';
 import { USERS_ADD } from './actionTypes';
 
 export const usersAdd = user => ({
   user,
   type: USERS_ADD,
 });
+
+export const usersLoad = uid =>
+  (dispatch, getState) => {
+    const { users } = getState();
+
+    if (!uid || users[uid]) {
+      return;
+    }
+
+    firebase.database().ref(`users/${uid}`).once('value').then((snapshot) => {
+      const user = {};
+      user[uid] = snapshot.val();
+
+      dispatch(usersAdd(user));
+    });
+  };
