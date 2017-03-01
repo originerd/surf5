be829da2fc35663f311c69032542469f65a8ec88
Jitae Kim <originerd@gmail.com>
2017-03-01 23:00:37 +0900

    Add upload surf logics to Write component

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,7 +10,13 @@ import {
 } from 'react-native';
 
 import colors from '../lib/colors';
+import firebase from '../lib/firebase';
 import Button from './Button';
+
+const propTypes = {
+  followers: PropTypes.object.isRequired,
+  navigationGoBack: PropTypes.func.isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -33,23 +42,90 @@ const styles = StyleSheet.create({
   },
 });
 
-const Write = () => (
-  <View style={styles.container}>
-    <TextInput
-      autoCapitalize="none"
-      autoFocus
-      blurOnSubmit={false}
-      placeholder="다섯 글자로 이야기를 전해보세요"
-      style={styles.input}
-    />
-    <Text style={styles.notice}>다섯 글자만 입력할 수 있습니다.</Text>
-    <Button
-      backgroundColor={colors.nerd}
-      block
-      color="whitesmoke"
-      text="올리기"
-    />
-  </View>
-);
+class Write extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      surf: '',
+    };
+
+    this.handleSurfChange = this.handleSurfChange.bind(this);
+    this.write = this.write.bind(this);
+  }
+
+  handleSurfChange(surf) {
+    this.setState({ surf });
+  }
+
+  get isAvailable() {
+    return /^[가-힣]{5}$/.test(this.state.surf);
+  }
+
+  write() {
+    this.setState({ isLoading: true });
+
+    const { followers, navigationGoBack } = this.props;
+    const uid = firebase.auth().currentUser.uid;
+    const surf = {
+      surf: this.state.surf,
+      timestamp: Date.now(),
+      uid,
+    };
+    const sid = firebase.database().ref().child('surfs').push().key;
+    const updates = {};
+
+    updates[`allSurfs/${sid}`] = surf;
+    updates[`likeCount/${sid}`] = 0;
+    updates[`surfs/${uid}/${sid}`] = surf;
+    Object.keys(followers).forEach((followerUID) => {
+      updates[`timeline/${followerUID}/${sid}`] = surf;
+    });
+
+    try {
+      firebase.database().ref().update(updates);
+      navigationGoBack();
+    } catch (e) {
+      console.log(e);
+      this.setState({ isLoading: false });
+    }
+  }
+
+  renderNotice() {
+    if (this.isAvailable) {
+      return null;
+    }
+
+    return <Text style={styles.notice}>한글 다섯 글자만 입력할 수 있습니다.</Text>;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCapitalize="none"
+          autoFocus
+          blurOnSubmit={false}
+          placeholder="다섯 글자로 이야기를 전해보세요"
+          onChangeText={this.handleSurfChange}
+          style={styles.input}
+        />
+        {this.renderNotice()}
+        <Button
+          backgroundColor={colors.nerd}
+          block
+          color="whitesmoke"
+          disabled={!this.isAvailable}
+          indicator={this.state.isLoading}
+          onPress={this.write}
+          text="올리기"
+        />
+      </View>
+    );
+  }
+}
+
+Write.propTypes = propTypes;
 
 export default Write;
--- src/containers/WriteScreen.js
+++ src/containers/WriteScreen.js
@@ -1,3 +1,16 @@
+import { connect } from 'react-redux';
+
+import { navigationGoBack } from '../actions/navigation';
 import Write from '../components/Write';
 
-export default Write;
+const mapStateToProps = state => ({
+  followers: state.followers,
+});
+
+const mapDispatchToProps = dispatch => ({
+  navigationGoBack() {
+    dispatch(navigationGoBack());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Write);
