a6c849213a1fc8c1ecef75410035b62d897051df
Jitae Kim <originerd@gmail.com>
2017-03-01 21:59:13 +0900

    Add followers reducer to combinedReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
+import followers from './followers';
 import navigation from './navigation';
 import tab from './tab';
 import timeline from './timeline';
 import users from './users';
 
 const reducers = combineReducers({
+  followers,
   navigation,
   tab,
   timeline,
